34211522d8ee280655c3b3a8be5e3d61fc4e37d2
Jay R SG <jaysen110@gmail.com>
2023-08-03 03:13:58 +0600

    User Profile

--- src/components/UserProfileView.js
+++ src/components/UserProfileView.js
@@ -1,26 +1,32 @@
-const UserProfileView = ({ user }) => {
+import { useAuthContext } from "@/contexts/authContext";
+import { useUserSearchContext } from "@/contexts/usersSearchContext";
+
+const UserProfileView = ({ }) => {
+  const { authData } = useAuthContext()
+  const { userSearchData } = useUserSearchContext()
+
   return (
     <div className=" mx-auto bg-white p-5 shadow-md rounded-md">
       <div className="flex flex-col items-center justify-center">
         <div className="flex-1/2 shadow-lg p-10">
           <h1 className="text-2xl font-bold text-blue-900">Personal Details</h1>
-          <h2 className="text-xl font-semibold">Name: Tahsin Mim</h2>
-          <p className="text-xl text-gray-600">ID: 18102015</p>
-          <p className="text-xl text-gray-600">Email ID: amitumi@gmail.com </p>
-          <p className="text-xl text-gray-600">Phone Number: 01711989745</p>
+          <h2 className="text-xl font-semibold">Name: {authData?.data?.first_name + " " + authData?.data?.last_name}</h2>
+          <p className="text-xl text-gray-600">ID: {authData?.data?.student_id}</p>
+          <p className="text-xl text-gray-600">Email ID: {authData?.data?.email} </p>
+          <p className="text-xl text-gray-600">Phone Number: {authData?.data?.phone}</p>
         </div>
 
         <div className="flex-1/2  shadow-lg p-10">
           <h1 className="text-2xl font-bold text-blue-900">Guardian Information</h1>
-          <p className="text-xl text-gray-600">Father's Name: Dad</p>
-          <p className="text-xl text-gray-600">Mother's Name: Mom</p>
-          <p className="text-xl text-gray-600">Phone Number: 01755874351</p>
+          <p className="text-xl text-gray-600">Father&apos;s Name: {authData?.data?.fathers_name ? authData?.data?.fathers_name : "N/A"}</p>
+          <p className="text-xl text-gray-600">Mother&apos;s Name: {authData?.data?.mothers_name ? authData?.data?.mothers_name : "N/A"}</p>
+          <p className="text-xl text-gray-600">Phone Number: {authData?.data?.guardian_phone ? authData?.data?.guardian_phone : ""}</p>
         </div>
 
         <div className="flex-1/2  shadow-lg p-10">
           <h1 className="text-2xl font-bold text-blue-900">RFID Card Information</h1>
-          <h2 className="text-xl font-semibold">Card Number : 5842153253261</h2>
-          <p className="text-xl text-gray-600">Balance: 1550TK</p>
+          <h2 className="text-xl font-semibold">Card Number : {authData?.data?.rfid}</h2>
+          <p className="text-xl text-gray-600">Balance: Tk.{userSearchData?.data?.balance}</p>
         </div>
       </div>
 
--- src/pages/student/index.js
+++ src/pages/student/index.js
@@ -2,9 +2,29 @@
 import { useUser } from "@/hooks/user";
 import SidebarUser from "../../components/SidebarUser";
 import UserProfileView from "../../components/UserProfileView";
+import { useUserSearchContext } from "@/contexts/usersSearchContext";
+import { useGetUsers } from "@/hooks/adminGetUsers";
+import { useEffect } from "react";
 
 const Profile = () => {
   const { data } = useUser({ middleware: "auth" })
+  const { data: searchData, error: searchError, isLoading, mutate } = useGetUsers(
+    {
+      postData: {
+        user_type: "student",
+      }
+    }
+  )
+
+  const { setUserSearchData } = useUserSearchContext()
+
+  useEffect(() => {
+    if (searchError) {
+      setUserSearchData(searchError?.respose?.data?.message)
+    } else if (searchData) {
+      setUserSearchData(searchData);
+    }
+  }, [searchData, searchError, setUserSearchData]);
 
   return (
     <div className="flex flex-row">
