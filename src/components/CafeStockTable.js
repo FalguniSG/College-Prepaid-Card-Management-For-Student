95722c46d4e08b543c13e2cffee106af77232c68
Jay R SG <jaysen110@gmail.com>
2023-08-03 04:24:07 +0600

    Get canteen items

--- src/components/CafeStockTable.js
+++ src/components/CafeStockTable.js
@@ -1,15 +1,34 @@
+import { useCanteenProds } from "@/hooks/canteen_prods";
+import { useState } from "react";
+
 const CafeStockTable = () => {
+  const [queryParams, setQueryParams] = useState({
+    search_term: "",
+  })
+
+  const [formData, setFormData] = useState({
+    name: "",
+    price: "",
+    quantity: ""
+  })
+
+  const { data: items } = useCanteenProds({ queryParams })
   return (
     <div className="rounded-sm border border-stroke bg-white m-4 px-5 pt-6 pb-2.5 shadow-default sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
-      <div className="flex-1 mr-2">
-        <input
-          type="text"
-          
-          className="block w-1/2 ml-[25%] mb-5 p-2 shadow-lg border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-          placeholder="Search Item..."
-        />
-      </div>
+        <div className="flex-1 mr-2">
+          <input
+            type="text"
+            name="search_term"
+            value={queryParams?.search_term}
+            onChange={(e) => {
+              setQueryParams({ ...queryParams, search_term: e.currentTarget.value })
+            }}
+
+            className="block w-1/2 ml-[25%] mb-5 p-2 shadow-lg border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+            placeholder="Search Item..."
+          />
+        </div>
         <table className="w-full table-auto">
           <thead>
             <tr className="bg-gray-2 text-left dark:bg-meta-4">
@@ -24,38 +43,40 @@ const CafeStockTable = () => {
           </thead>
 
           <tbody>
-            <tr>
-              <td className="border-b text-left border-[#eee] ">
-                <p className="text-black  px-8"> Biryani</p>
-              </td>
+            {items?.data?.map((item, ind) => (
+              <tr key={"item" + ind}>
+                <td className="border-b text-left border-[#eee] ">
+                  <p className="text-black  px-8"> {item?.name}</p>
+                </td>
 
-              <td className="border-b border-[#eee]  px-10">
-                <p className="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success">
-                  200
-                </p>
-              </td>
-              <td className="border-b border-[#eee] px-10">
-                <p className="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success">
-                  30
-                </p>
-              </td>
-              <td className="border-b border-[#eee] px-10">
-                <button
-                  type="button"
-                  className="w-full bg-blue-500 text-white py-2 px-1 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-                >
-                  Edit
-                </button>
-              </td>
-              <td className="border-b border-[#eee] ">
-                <button
-                  type="button"
-                  className="w-full bg-blue-500 text-white py-2 px-1 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-                >
-                  Update
-                </button>
-              </td>
-            </tr>
+                <td className="border-b border-[#eee]  px-10">
+                  <p className="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success">
+                    {item?.price}
+                  </p>
+                </td>
+                <td className="border-b border-[#eee] px-10">
+                  <p className="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success">
+                    {item?.quantity}
+                  </p>
+                </td>
+                <td className="border-b border-[#eee] px-10">
+                  <button
+                    type="button"
+                    className="w-full bg-blue-500 text-white py-2 px-1 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td className="border-b border-[#eee] ">
+                  <button
+                    type="button"
+                    className="w-full bg-blue-500 text-white py-2 px-1 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                  >
+                    Update
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
