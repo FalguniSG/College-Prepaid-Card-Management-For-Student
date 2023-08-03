871cea55db6d0714b33eec3f20ecac51dfa962da
Jay R SG <jaysen110@gmail.com>
2023-08-03 08:22:05 +0600

    recharge history

--- src/components/RechargeHistory.js
+++ src/components/RechargeHistory.js
@@ -1,54 +1,64 @@
+import { useAuthContext } from "@/contexts/authContext";
+import { useUserSearchContext } from "@/contexts/usersSearchContext";
 import { useUserRechargeHistory } from "@/hooks/recharge_history";
+import { useEffect } from "react";
 
 const RechargeHistory = () => {
   const { data, isLoading, mutate } = useUserRechargeHistory()
+  const { authData } = useAuthContext()
+  const { userSearchData } = useUserSearchContext()
+
+
 
   return (
-    <div className="rounded-sm border border-stroke bg-white m-4 px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
-      <div className="max-w-full overflow-x-auto">
-        <table className="w-full table-auto">
-          <thead>
-            <tr className="bg-gray-2 text-left dark:bg-meta-4">
-              <th className=" py-4 px-8 font-medium text-black  xl:pl-11">
-                Date & Time
-              </th>
-              <th className="py-4 px-4 font-medium text-black ">
-                Amount
-              </th>
-              {/* <th className="py-4 px-4 font-medium text-black ">
+    <>
+      <h1 className="pl-5 font-black">Recharge History</h1>
+      <div className="rounded-sm border border-stroke bg-white m-4 px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+        <div className="max-w-full overflow-x-auto">
+          <table className="w-full table-auto">
+            <thead>
+              <tr className="bg-gray-2 text-left dark:bg-meta-4">
+                <th className=" py-4 px-8 font-medium text-black  xl:pl-11">
+                  Date & Time
+                </th>
+                <th className="py-4 px-4 font-medium text-black ">
+                  Amount
+                </th>
+                {/* <th className="py-4 px-4 font-medium text-black ">
                 Remaining Balance
               </th> */}
-            </tr>
-          </thead>
+              </tr>
+            </thead>
 
 
-          <tbody>
-            {data?.data?.map((data, ind) => (
+            <tbody>
+              {data?.data?.map((data, ind) => (
 
-              <tr key={"rech" + ind}>
+                <tr key={"rech" + ind}>
 
-                <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
-                  <h5 className="font-medium text-black dark:text-gray">
-                    {data?.created_at.split(" ")[0]}
-                  </h5>
-                  <p className="text-sm">{data?.created_at.split(" ")[1]}</p>
-                </td>
-                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                  <p className="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success">
-                    {data?.recharge_amount}
-                  </p>
-                </td>
-                {/* <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
+                    <h5 className="font-medium text-black dark:text-gray">
+                      {data?.created_at.split(" ")[0]}
+                    </h5>
+                    <p className="text-sm">{data?.created_at.split(" ")[1]}</p>
+                  </td>
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <p className="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success">
+                      {data?.recharge_amount}
+                    </p>
+                  </td>
+                  {/* <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                 <p className="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-success">
                   1660
                 </p>
               </td> */}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
